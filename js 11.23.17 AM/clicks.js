77cf81a41e915ec7bc5339e8ced4ac971f70ccb8
Matt Silveira <msilveira@tnc.org>
2019-03-08 11:23:42 -0700

    push code from dev

---
+++ js 11.23.17 AM/clicks.js
@@ -0,0 +1,191 @@
+define([
+	"dojo/_base/declare", "esri/tasks/query", "esri/tasks/QueryTask", "esri/layers/FeatureLayer", "esri/dijit/Search", "esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color"
+],
+function ( declare, Query, QueryTask,FeatureLayer, Search, SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color) {
+        "use strict";
+
+        return declare(null, {
+			eventListeners: function(t){
+				$('#' + t.id + 'viewRankingText').on('click', function(v){
+					let text = v.currentTarget.textContent;
+					if(text == 'View Ranking Thresholds'){
+						$('.risk-rankingThresholdText').slideDown();
+						$(v.currentTarget).html('Hide Ranking Thresholds')
+						$(v.currentTarget).css('color', 'rgb(140, 33, 48)')
+					}else{
+						$('.risk-rankingThresholdText').slideUp();
+						$(v.currentTarget).html('View Ranking Thresholds');
+						$(v.currentTarget).css('color', '#2f6384');
+					}
+				})
+				// main rad buttons click
+				$('.risk-resultsRadBtns input').on('click', function(evt){
+					t.obj.visibleLayers = []
+					$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+						$(v).attr('disabled', true);
+					})
+					if(evt.currentTarget.value == 'final'){
+						let layer;
+						if(t.obj.waterRiseVal == 1){
+							layer = 15
+						}else if(t.obj.waterRiseVal == 2){
+							layer = 16
+						}else if(t.obj.waterRiseVal == 3){
+							layer = 17
+						}else if(t.obj.waterRiseVal == 4){
+							layer = 18
+						}
+						t.obj.visibleLayers.push(layer)
+						t.obj.viewResultsTracker = 'final'
+						$('.risk-waterRiseWrapper').slideDown()
+					}else if(evt.currentTarget.value == 'combExp'){
+						let layer;
+						if(t.obj.waterRiseVal == 1){
+							layer = 11
+						}else if(t.obj.waterRiseVal == 2){
+							layer = 12
+						}else if(t.obj.waterRiseVal == 3){
+							layer = 13
+						}else if(t.obj.waterRiseVal == 4){
+							layer = 14
+						}
+						t.obj.visibleLayers.push(layer)
+						t.obj.viewResultsTracker = 'combExp'
+						$('.risk-waterRiseWrapper').slideDown()
+					}else if(evt.currentTarget.value == 'combVul'){
+						t.obj.visibleLayers.push(10)
+						t.obj.viewResultsTracker = 'combVul'
+						$('.risk-waterRiseWrapper').slideUp()
+					}else if(evt.currentTarget.value == 'individual'){
+						$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+							$(v).attr('disabled', false);
+						})
+						let layerVal;
+						if(t.obj.wetlandVal == 'coastalFlood'){
+							if (t.obj.waterRiseVal ==1 ) {
+								layerVal = 6
+							}else if(t.obj.waterRiseVal == 2){
+								layerVal = 7
+							}else if(t.obj.waterRiseVal == 3){
+								layerVal = 8
+							}else if(t.obj.waterRiseVal == 4){
+								layerVal = 9
+							}
+							t.obj.visibleLayers.push(layerVal)
+							$('.risk-waterRiseWrapper').slideDown()
+						}else{
+							t.obj.visibleLayers.push(t.layersNameArray.indexOf(t.obj.wetlandVal))
+							$('.risk-waterRiseWrapper').slideUp()
+						}
+						t.obj.viewResultsTracker = 'individual'
+					}
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				})
+
+				// on individual radio button click
+				$('.risk-wetlandsSuitWrapper input').on('click', function(evt){
+					t.obj.visibleLayers = []
+					t.obj.wetlandVal = evt.currentTarget.value;
+					if(t.obj.wetlandVal == 'coastalFlood'){
+						// display the layer based on the water rise slider
+						let layerVal;
+						if (t.obj.waterRiseVal ==1 ) {
+							layerVal = 6
+						}else if(t.obj.waterRiseVal == 2){
+							layerVal = 7
+						}else if(t.obj.waterRiseVal == 3){
+							layerVal = 8
+						}else if(t.obj.waterRiseVal == 4){
+							layerVal = 9
+						}
+						t.obj.visibleLayers.push(layerVal)
+						// slide up slider bar
+						$('.risk-waterRiseWrapper').slideDown()
+					}else{
+						// display layer based on the value passed
+						t.obj.visibleLayers.push(t.layersNameArray.indexOf(t.obj.wetlandVal))
+						// slide up slider bar
+						$('.risk-waterRiseWrapper').slideUp()
+					}
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				})
+				// on water rise slide
+				$("#" + t.id + "sldr").on('slide', function(v, ui){
+					t.obj.waterRiseVal = ui.value
+					t.obj.visibleLayers = []
+					if(t.obj.viewResultsTracker == 'final'){
+						let layer;
+						if(t.obj.waterRiseVal == 1){
+							layer = 15
+						}else if(t.obj.waterRiseVal == 2){
+							layer = 16
+						}else if(t.obj.waterRiseVal == 3){
+							layer = 17
+						}else if(t.obj.waterRiseVal == 4){
+							layer = 18
+						}
+						t.obj.visibleLayers.push(layer)
+					}else if(t.obj.viewResultsTracker == 'individual' && t.obj.wetlandVal  =='coastalFlood'){
+						let layerVal;
+						if (t.obj.waterRiseVal ==1 ) {
+							layerVal = 6
+						}else if(t.obj.waterRiseVal == 2){
+							layerVal = 7
+						}else if(t.obj.waterRiseVal == 3){
+							layerVal = 8
+						}else if(t.obj.waterRiseVal == 4){
+							layerVal = 9
+						}
+						t.obj.visibleLayers.push(layerVal)
+					}else if(t.obj.viewResultsTracker == 'combExp'){
+						let layerVal;
+						if (t.obj.waterRiseVal ==1 ) {
+							layerVal = 11
+						}else if(t.obj.waterRiseVal == 2){
+							layerVal = 12
+						}else if(t.obj.waterRiseVal == 3){
+							layerVal = 13
+						}else if(t.obj.waterRiseVal == 4){
+							layerVal = 14
+						}
+						t.obj.visibleLayers.push(layerVal)
+					}
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				})
+
+				// $('#' + t.id + 'viewChangeText').on('click', function(v){
+				// 	let text = v.currentTarget.textContent;
+				// 	if(text == 'View Change Scenarios'){
+				// 		$('.risk-floodText').slideDown();
+				// 		$(v.currentTarget).html('Hide Change Scenarios')
+				// 		$(v.currentTarget).css('color', 'rgb(140, 33, 48)')
+				// 	}else{
+				// 		$('.risk-floodText').slideUp();
+				// 		$(v.currentTarget).html('View Change Scenarios');
+				// 		$(v.currentTarget).css('color', '#2f6384');
+				// 	}
+				// })
+
+			},
+			
+			commaSeparateNumber: function(val){
+				while (/(\d+)(\d{3})/.test(val.toString())){
+					val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+				}
+				return val;
+			},
+			abbreviateNumber: function(num) {
+			    if (num >= 1000000000) {
+			        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
+			     }
+			     if (num >= 1000000) {
+			        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+			     }
+			     if (num >= 1000) {
+			        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+			     }
+			     return num;
+			}
+        });
+    }
+);
---
+++ js 11.23.17 AM/esriapi.js
@@ -0,0 +1,172 @@
+define([
+	"esri/layers/ArcGISDynamicMapServiceLayer", "esri/geometry/Extent", "esri/SpatialReference", "esri/tasks/query" ,"esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", 
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color"
+],
+function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, QueryTask, declare, FeatureLayer, 
+			SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color ) {
+        "use strict";
+
+        return declare(null, {
+			esriApiFunctions: function(t){	
+				// Add dynamic map service
+				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.7});
+				t.map.addLayer(t.dynamicLayer);
+				if (t.obj.visibleLayers.length > 0){	
+					t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+				}
+				t.map.setMapCursor("pointer");
+				t.map.on("zoom-end", function(evt){
+					t.map.setMapCursor("pointer");
+				});
+				t.dynamicLayer.on("load", function () {
+					// if save and share
+					if(t.obj.stateSet == 'yes'){
+						t.dynamicLayer.setOpacity(t.obj.opacitySliderVal/100);
+
+						// figure out which main radio button needs to be checked
+						$.each($('.risk-resultsRadBtns input'), function(i,v){
+							if(v.value == t.obj.viewResultsTracker){
+								$(v).attr('checked', true);
+							}else{
+								$(v).attr('checked', false);
+							}
+						})
+						// find out which wetlands radio button needs to be checked
+						$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+							if(v.value == t.obj.wetlandVal){
+								$(v).attr('checked', true);
+							}else{
+								$(v).attr('checked', false);
+							}
+						})
+						// handle when the individula rad buttons are disabled
+						if(t.obj.viewResultsTracker == 'final'){
+							$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+								$(v).attr('disabled', true)
+							})
+							$('.risk-waterRiseWrapper').slideDown()
+
+						}else if(t.obj.viewResultsTracker == 'individual'){
+							$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+								$(v).attr('disabled', false)
+							})
+							if(t.obj.wetlandVal == 'coastalFlood'){
+								$('.risk-waterRiseWrapper').slideDown()
+							}else{
+								$('.risk-waterRiseWrapper').slideUp()
+							}
+						}else if(t.obj.viewResultsTracker == 'combExp'){
+							$('.risk-waterRiseWrapper').slideUp()
+
+							$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+								$(v).attr('disabled', true)
+							})
+						}else if(t.obj.viewResultsTracker == 'combVul'){
+							$('.risk-waterRiseWrapper').slideUp()
+
+							$.each($('.risk-wetlandsSuitWrapper input'), function(i,v){
+								$(v).attr('disabled', true)
+							})
+						}
+
+
+
+					}else{
+						t.map.setExtent(t.dynamicLayer.fullExtent.expand(1.2), true)
+					}
+
+
+
+					
+					// get layers array, set extent, change map cursor 			
+					t.layersArray = t.dynamicLayer.layerInfos;
+					t.map.setMapCursor("pointer");
+					t.map.on("zoom-end", function(evt){
+						t.map.setMapCursor("pointer");
+					});
+
+					// build a name layers array
+					t.layersNameArray = []
+					$.each(t.layersArray, function(i,v){
+						t.layersNameArray.push(v.name);
+					})
+				});
+				// opacity slider
+				$(function() {
+				    $("#" + t.id + "opacity-slider").slider({ min: 1, max: 100, range: false, values:[t.obj.opacitySliderVal] })
+				    // on opacity slide
+				    $("#" + t.id + "opacity-slider").on('slide', function(v,ui){
+				    	t.obj.opacitySliderVal = ui.value
+				  		t.dynamicLayer.setOpacity(ui.value/100); // set init opacity
+				    })
+				});
+
+				$(function() {
+				  $("#" + t.id + "sldr").slider({ min: 1, max: 4, range: false, values:[t.obj.waterRiseVal] })
+				    // .slider("pips", { rest: "label"})
+				    // .slider("float");
+				});
+				$(function() {
+				  $("#" + t.id + "sldr2").slider({ min: 1, max: 3, range: false, })
+				    // .slider("pips", { rest: "label"})
+				    // .slider("float");
+				});
+
+// // Dynamic layer on load ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 				t.dynamicLayer.on("load", function () { 			
+// 					t.layersArray = t.dynamicLayer.layerInfos;
+					
+// 					// trigger clicks on techniques
+// 					$.each($('#' + t.id + 'funcWrapper input'),function(i,v){
+// 						if (t.obj.enhanceTech == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+// 					// trigger clicks on sup data radio buttons
+// 					$.each($('#' + t.id + 'aus-supDataWrap input'),function(i,v){
+// 						if (t.obj.supData == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+// 					// trigger initial top control clicks
+// 					$.each($('#' + t.id + 'aus-viewRadioWrap input'),function(i,v){
+// 						if (t.obj.viewRadCBCh == v.value){
+// 							$('#' + v.id).trigger('click');	
+// 						}	
+// 					});
+
+// 					var extent = new Extent(144.1, -38.6, 145.7, -37.6, new SpatialReference({ wkid:4326 }))
+// 					if (t.obj.stateSet == "no"){
+// 						// t.map.setExtent(t.dynamicLayer.fullExtent.expand(1.2), true)
+// 						t.map.setExtent(extent, true)
+// 					}
+// // Save and Share ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 					// Save and Share Handler					
+// 					if (t.obj.stateSet == "yes"){
+// 						// // set slider values
+// 						// $.each(t.obj.slIdsVals,function(i,v){
+// 						// 	$('#' + t.id + v[0]).slider('values', v[1]);
+// 						// });	
+// 						// // checkboxes for sliders
+// 						// $.each(t.obj.slCbIds,function(i,v){
+// 						// 	$('#' + t.id + v).trigger('click');
+// 						// })
+// 						// // set radio buttons to checked state
+// 						// $.each(t.obj.rbIds,function(i,v){
+// 						// 	$('#' + t.id + v).attr('checked', true);
+// 						// })
+// 						// // checkboxes for radio buttons
+// 						// $.each(t.obj.rbCbIds,function(i,v){
+// 						// 	$('#' + t.id + v).trigger('click');
+// 						// })
+// 						// //extent
+// 						// var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
+// 						// t.map.setExtent(extent, true);
+// 						// t.obj.stateSet = "no";
+// 					}	
+					
+// 				});					
+			}
+		});
+    }
+);
