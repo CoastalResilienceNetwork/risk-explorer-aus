6a2fe94fca848b9f89b26b6550beabb2727fa91e
Matt Silveira <msilveira@tnc.org>
2019-08-26 10:30:18 -0400

    change url again

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, conte
 			declare.safeMixin(this, frameworkParameters);
 			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
 			this.obj = dojo.eval("[" + obj + "]")[0];	
-			this.url = "https://dev-services.coastal-resilience.net:6080/arcgis/rest/services/Australia/Risk_Explorer/MapServer";
+			this.url = "https://dev-services.coastalresilience.org/arcgis/rest/services/Australia/Risk_Explorer/MapServer";
 			this.layerDefs = [];
 		},
 		// Called after initialize at plugin startup (why the tests for undefined). Also called after deactivate when user closes app by clicking X. 
